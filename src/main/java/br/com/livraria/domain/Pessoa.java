27cdd97a38559dac82903d11bced8d211953481f
Everton Ribeiro <everton@Everton.Everton>
2017-06-30 06:18:20 -0400

    ex. JSF CDI

---
+++ src/main/java/br/com/livraria/domain/Pessoa.java
@@ -0,0 +1,70 @@
+package br.com.livraria.domain;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable{
+
+	/**
+	 * Serializando obj
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private String email;
+	
+	public Pessoa(){}
+	
+	public Integer getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
+
+}
